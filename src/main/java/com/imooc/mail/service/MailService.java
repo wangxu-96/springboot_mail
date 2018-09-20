967f8fe237f056046f0985e090e0f98fa7100316
ouakira <1638897848@qq.com>
2018-09-20 15:03:56 +0800

    complete

--- src/main/java/com/imooc/mail/service/MailService.java
+++ src/main/java/com/imooc/mail/service/MailService.java
@@ -1,7 +1,10 @@
 package com.imooc.mail.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMailMessage;
@@ -10,9 +13,12 @@ import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @Service
 public class MailService {
+    private final Logger logger= LoggerFactory.getLogger(this.getClass());
+
     @Value("${spring.mail.username}")
     private String from;
 
@@ -40,5 +46,42 @@ public class MailService {
         helper.setText(content,true);
         helper.setFrom(from);
         mailSender.send(message);
+    }
+
+    public void sendAttachmentsMail(String to,String subject,String content,String filePath) throws MessagingException {
+        MimeMessage message=mailSender.createMimeMessage();
+        MimeMessageHelper helper=new MimeMessageHelper(message,true);
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(content);
+
+        FileSystemResource file =new FileSystemResource(new File(filePath));
+        String fileName=file.getFilename();
+
+        helper.addAttachment(fileName,file);
+        mailSender.send(message);
+    }
+
+    public void sendInlineResourceMail(String to,String subject,String content,String rscPath,String rscId)  {
+
+        logger.info("send starting....");
+        MimeMessage message=mailSender.createMimeMessage();
+        MimeMessageHelper helper;
+        try {
+            helper = new MimeMessageHelper(message,true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content,true);
+            helper.setFrom(from);
+            FileSystemResource file=new FileSystemResource(new File(rscPath));
+
+            helper.addInline(rscId,file);
+            logger.info("send successful");
+        } catch (MessagingException e) {
+            logger.error("send failed....");
+        }
+        mailSender.send(message);
+
     }
 }
--- src/test/java/com/imooc/mail/service/ServiceTest.java
+++ src/test/java/com/imooc/mail/service/ServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import javax.annotation.Resource;
 import javax.mail.MessagingException;
@@ -13,6 +15,8 @@ import javax.mail.MessagingException;
 public class ServiceTest {
     @Resource
     MailService mailService;
+    @Resource
+    TemplateEngine templateEngine;
     @Test
     public void sayHelloTest(){
         mailService.sayHello();
@@ -30,4 +34,26 @@ public class ServiceTest {
                 "</html>";
         mailService.sendHtmlMail("18375310856@163.com","html mail spring boot",content);
     }
+
+    @Test
+    public void sendAttachmentsMailTest() throws MessagingException {
+        mailService.sendAttachmentsMail("18375310856@163.com","file  spring boot","spring_boot_first_mail","/Users/ouakira/go/src/learninggo/src/job/Pointer.go");
+    }
+
+    @Test
+    public void sendInlineResourceMailTest() throws MessagingException {
+        String id="001";
+        String filePath="/Users/ouakira/Downloads/Collection.jpg";
+        String content="<html><body><img src=\'cid:"+id+"\'></img></body></html>";
+        mailService.sendInlineResourceMail("18375310856@163.com","picture  spring boot",content,filePath,id);
+    }
+    @Test
+    public void testTemplatesMailTest() throws MessagingException {
+        Context context=new Context();
+        context.setVariable("id","007");
+        String emailContent=templateEngine.process("emailTemplate",context);
+        mailService.sendHtmlMail("18375310856@163.com","html mail spring boot",emailContent);
+    }
+
+
 }
